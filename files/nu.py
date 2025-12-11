1aceb21b20b90a3132a2bfef7716df1c52e6b068
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-12-11 10:31:43 -0600

    Fix: attempt to use more accurate lighting flag

--- files/nu.py
+++ files/nu.py
@@ -40,7 +40,7 @@ class NuMaterial:
 
         self.is_alpha_blended = (attributes & 0xF) != 0
 
-        self.is_emissive = (attributes & 0x2) != 0
+        self.is_emissive = (attributes & 0x30000) != 0x20000
 
         self.diffuse = NuColour3(
             data, offset + 0x50 + NuMaterial.PLATFORM_OFFSETS[platform]
