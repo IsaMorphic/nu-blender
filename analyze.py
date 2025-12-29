cabcf8ef2079c5e60cbbaa0b17257b18d45e6e4b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-12-29 18:30:49 +0100

    Feature: add support for Xbox files to analysis script (#14)
    This PR introduces a minimal set of changes to the original analysis script to add support for evaluating either the PC or Xbox version of the game.
    
    Reviewed-on: https://codeberg.org/leftmostcat/nu-blender/pulls/14
    Reviewed-by: leftmostcat <leftmostcat@noreply.codeberg.org>
    Co-authored-by: Isabelle Santin <isabelle@chosenfewsoftware.com>
    Co-committed-by: Isabelle Santin <isabelle@chosenfewsoftware.com>

--- analyze.py
+++ analyze.py
@@ -5,41 +5,72 @@ from files.nu import NuPlatform
 from files.nup import Nup
 
 
+class PlatformInfo:
+    PLATFORM_INFO = {
+        "pc": {
+            "ext": ".nup",
+            "platform": NuPlatform.PC,
+        },
+        "xbox": {
+            "ext": ".nux",
+            "platform": NuPlatform.XBOX,
+        },
+    }
+
+    def __init__(self, platform: str):
+        platform_info = PlatformInfo.PLATFORM_INFO[platform]
+        self.ext = platform_info["ext"]
+        self.platform = platform_info["platform"]
+
+
 def main():
     parser = argparse.ArgumentParser(prog="nu-analyze")
     parser.add_argument("data_path")
+    parser.add_argument("--platform", choices=["xbox", "pc"], required=True)
 
     args = parser.parse_args()
 
-    nups = scan_dir(args.data_path)
+    platform_info = PlatformInfo(args.platform)
+    nup_paths = scan_dir(args.data_path, platform_info.ext)
 
     alpha_modes = {}
     alpha_tests = {}
     colours = {}
     effect_ids = {}
     lightings = {}
-    for nup in nups:
-        with open(nup, "rb") as file:
+    for nup_path in nup_paths:
+        nup_name = os.path.basename(nup_path)
+        with open(nup_path, "rb") as file:
             data = file.read()
-            nup = Nup(data, NuPlatform.PC)
+            try:
+                nup = Nup(data, platform_info.platform)
+            except:
+                print("Failed to parse: {}".format(nup_name))
+                continue
 
-            for material in nup.materials:
-                alpha_mode = material.alpha_mode()
+            for i, material in enumerate(nup.materials):
+                try:
+                    alpha_mode = material.alpha_mode()
+                    alpha_test = material.alpha_test()
+                    colour = material.colour()
+                    effect_id = material.effect_id
+                    lighting = material.lighting()
+                except:
+                    print("Failed to analyze material #{} in: {}".format(i, nup_name))
+                    continue
+
                 alpha_modes.setdefault(alpha_mode, 0)
                 alpha_modes[alpha_mode] += 1
 
-                alpha_test = material.alpha_test()
                 alpha_tests.setdefault(alpha_test, 0)
                 alpha_tests[alpha_test] += 1
 
-                colour = material.colour()
                 colours.setdefault(colour, 0)
                 colours[colour] += 1
 
-                effect_ids.setdefault(material.effect_id, 0)
-                effect_ids[material.effect_id] += 1
+                effect_ids.setdefault(effect_id, 0)
+                effect_ids[effect_id] += 1
 
-                lighting = material.lighting()
                 lightings.setdefault(lighting, 0)
                 lightings[lighting] += 1
 
@@ -50,14 +81,14 @@ def main():
     print("fxid: {}".format(effect_ids))
 
 
-def scan_dir(path):
+def scan_dir(path: str, match_ext: str):
     nups = []
     for entry in os.scandir(path):
         if entry.is_dir():
-            nups += scan_dir(entry.path)
+            nups += scan_dir(entry.path, match_ext)
         elif entry.is_file():
             _, ext = os.path.splitext(entry.path)
-            if ext == ".nup":
+            if ext.lower() == match_ext.lower():
                 nups.append(entry.path)
 
     return nups
